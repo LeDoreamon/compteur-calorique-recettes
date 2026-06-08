4620bacee2b92f7fbcfc866843f5dcedf7d2e178
LeDoreamon <doreamon51100@gmail.com>
2026-06-08 17:59:55 +0200

    SW 2026-06-08 17h59

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// build 2026-06-08 11h31
-const CACHE='macros-2026-06-08-1131';
+// build 2026-06-08 17h59
+const CACHE='macros-2026-06-08-1759';
 self.addEventListener('install',e=>{self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.map(x=>caches.delete(x)))).then(()=>self.clients.matchAll({type:'window',includeUncontrolled:true})).then(cs=>cs.forEach(c=>c.navigate&&c.navigate(c.url))));self.clients.claim();});
 self.addEventListener('fetch',e=>{if(e.request.url.includes('api.groq.com')||e.request.url.includes('firebasedatabase.app'))return;e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r;}).catch(()=>caches.match(e.request)));});
