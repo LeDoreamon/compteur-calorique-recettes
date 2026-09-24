e3463d14381dfc3fecbf1492969c3fe9e105621c
LeDoreamon <doreamon51100@gmail.com>
2026-09-25 01:29:12 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// build 2026-09-24 23h05
-const CACHE='macros-2026-09-24-2305';
+// build 2026-09-25 01h35
+const CACHE='macros-2026-09-25-0135';
 self.addEventListener('install',e=>{self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{if(e.request.url.includes('api.groq.com')||e.request.url.includes('firebasedatabase.app')||e.request.url.includes('openfoodfacts.org'))return;e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r;}).catch(()=>caches.match(e.request)));});
